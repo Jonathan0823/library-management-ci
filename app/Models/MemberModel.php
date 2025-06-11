d948ac8c8f768b02fe4daaa11f7fb87b2b94c301
Guinn <eguinjonathan08@gmail.com>
2025-06-11 19:35:07 +0700

    feat(member): add validation to the model

--- app/Models/MemberModel.php
+++ app/Models/MemberModel.php
@@ -16,11 +16,31 @@ class MemberModel extends Model
         'phone',
         'email',
     ];
-    protected $useTimestamps      = true;
+    protected $useTimestamps = true;
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
 
-    protected $validationRules    = [];
-    protected $validationMessages = [];
+    protected $validationRules    = [
+          'name'    => 'required|min_length[3]|max_length[100]',
+          'address' => 'permit_empty|max_length[255]',
+          'phone'   => 'permit_empty|regex_match[/^\+?[0-9\s\-()]+$/]',
+          'email'   => 'permit_empty|valid_email',
+    ];
+    protected $validationMessages = [
+          'name' => [
+              'required'    => 'Name is required.',
+              'min_length'  => 'Name must be at least 3 characters long.',
+              'max_length'  => 'Name cannot exceed 100 characters.',
+          ],
+          'address' => [
+              'max_length'  => 'Address cannot exceed 255 characters.',
+          ],
+          'phone' => [
+              'regex_match' => 'Phone number format is invalid.',
+          ],
+          'email' => [
+              'valid_email' => 'Email address is not valid.',
+          ],
+    ];
     protected $skipValidation     = false;
 }
